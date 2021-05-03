fefc21d13a3f7eebecace16240a4d8bf6a5c0e40
D1m0nd <70190542+D1maD1m0nd@users.noreply.github.com>
2021-05-03 19:15:20 +0300

    Task#3 : финальная версия, добавлен поворот экрана

--- app/src/main/java/com/example/lesson1/MainActivity.java
+++ app/src/main/java/com/example/lesson1/MainActivity.java
@@ -9,7 +9,6 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-import java.util.Locale;
 
 import static com.example.lesson1.CalculatorConstants.CLEAR;
 import static com.example.lesson1.CalculatorConstants.REGEX_IS_SYMBOL_OPERATION;
