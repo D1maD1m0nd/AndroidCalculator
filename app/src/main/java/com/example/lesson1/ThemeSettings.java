711b91118648a9cba62fe8353070bc7a95ca2d4d
D1m0nd <70190542+D1maD1m0nd@users.noreply.github.com>
2021-05-11 22:15:00 +0300

    add style for buttons

---
+++ app/src/main/java/com/example/lesson1/ThemeSettings.java
@@ -0,0 +1,14 @@
+package com.example.lesson1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ThemeSettings extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_theme_settings);
+    }
+}
